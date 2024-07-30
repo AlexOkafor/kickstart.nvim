4e582606c343fddcb5b533b59cec6ce13c442da1
Alex Okafor <alex@paradeofrain.com>
2024-07-30 09:03:06 -0400

    feat: add more theme integrations

--- lua/custom/plugins/theme.lua
+++ lua/custom/plugins/theme.lua
@@ -3,4 +3,18 @@ return {
   name = 'catppuccin',
   priority = 1000,
   opts = { flavour = 'mocha' },
+  config = function()
+    local cat = require 'catppuccin'
+    cat.setup {
+      integrations = {
+        telescope = {
+          enabled = true,
+        },
+        cmp = true,
+        gitsigns = true,
+        neotest = true,
+        neotree = true,
+      },
+    }
+  end,
 }
