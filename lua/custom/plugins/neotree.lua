0526e7b86c8915bc9219dcfc479cdd96b4974cbd
Alex Okafor <alex@paradeofrain.com>
2024-07-30 08:14:49 -0400

    feat: got neotree working

---
+++ lua/custom/plugins/neotree.lua
@@ -0,0 +1,21 @@
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  branch = 'v3.x',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+    -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+  },
+  buffers = {
+    follow_current_file = { enabled = true },
+    leave_dirs_open = false,
+  },
+  config = function()
+    vim.fn.sign_define('DiagnosticSignError', { text = ' ', texthl = 'DiagnosticSignError' })
+    vim.fn.sign_define('DiagnosticSignWarn', { text = ' ', texthl = 'DiagnosticSignWarn' })
+    vim.fn.sign_define('DiagnosticSignInfo', { text = ' ', texthl = 'DiagnosticSignInfo' })
+    vim.fn.sign_define('DiagnosticSignHint', { text = '󰌵', texthl = 'DiagnosticSignHint' })
+    vim.keymap.set('n', '<leader>e', '<CMD>Neotree toggle reveal<CR>', { noremap = true, silent = true, desc = 'Toggle Neotree' })
+  end,
+}
