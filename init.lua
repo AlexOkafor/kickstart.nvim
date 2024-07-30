3122684b21bdad9ebd3af3a433c053c9838800c6
Alex Okafor <alex@paradeofrain.com>
2024-07-29 22:21:12 -0400

    fix: remove RA conflict

--- init.lua
+++ init.lua
@@ -618,7 +618,7 @@ require('lazy').setup({
         },
 
         -- pyright = {},
-        rust_analyzer = {},
+        -- rust_analyzer = {}, -- disablin this due to rustaceanvim controlling the LSP
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
