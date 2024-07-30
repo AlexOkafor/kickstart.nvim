3ed3535e478dcfc8c1eeba1b0f1887a1e7ff9508
Alex Okafor <alex@paradeofrain.com>
2024-07-30 06:52:06 -0400

    feat: stop conflict with rust RA

--- init.lua
+++ init.lua
@@ -663,6 +663,9 @@ require('lazy').setup({
       require('mason-lspconfig').setup {
         handlers = {
           function(server_name)
+            if server_name == 'rust_analyzer' then
+              return true
+            end
             local server = servers[server_name] or {}
             -- This handles overriding only values explicitly passed
             -- by the server configuration above. Useful when disabling
