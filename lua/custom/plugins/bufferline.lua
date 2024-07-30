113562aa4289398860eb65efbad1daa838a1251b
Alex Okafor <alex@paradeofrain.com>
2024-07-30 07:02:52 -0400

    feat: get buffers to close based off bindings using bufferline

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -4,5 +4,8 @@ return {
   dependencies = 'nvim-tree/nvim-web-devicons',
   config = function()
     require('bufferline').setup {}
+
+    vim.keymap.set('n', '<leader>bo', '<CMD>BufferLineCloseOthers<CR>', { noremap = true, silent = true, desc = 'Close Other Buffers' })
+    vim.keymap.set('n', '<leader>br', '<CMD>BufferLineCloseRight<CR>', { noremap = true, silent = true, desc = 'Close Buffers To The Right' })
   end,
 }
