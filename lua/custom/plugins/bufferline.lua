6a427e45a7fe6223ebf308c3d88a80a0321f4dec
Alex Okafor <alex@paradeofrain.com>
2024-07-30 07:06:46 -0400

    fix: formatting

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -4,7 +4,6 @@ return {
   dependencies = 'nvim-tree/nvim-web-devicons',
   config = function()
     require('bufferline').setup {}
-
     vim.keymap.set('n', '<leader>bo', '<CMD>BufferLineCloseOthers<CR>', { noremap = true, silent = true, desc = 'Close Other Buffers' })
     vim.keymap.set('n', '<leader>br', '<CMD>BufferLineCloseRight<CR>', { noremap = true, silent = true, desc = 'Close Buffers To The Right' })
   end,
