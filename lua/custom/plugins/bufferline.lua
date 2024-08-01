9e2c48b895ea41c1657bf9c92036e375528986df
Alex Okafor <alex@paradeofrain.com>
2024-08-01 08:20:43 -0400

    feat: make bufferline ignore neotree and don't go back to tree

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -3,7 +3,25 @@ return {
   version = '*',
   dependencies = 'nvim-tree/nvim-web-devicons',
   config = function()
-    require('bufferline').setup {}
+    local util = require 'util'
+    require('bufferline').setup {
+      options = {
+        close_command = function(n)
+          util.bufremove(n)
+        end,
+        right_mouse_command = function(n)
+          util.bufremove(n)
+        end,
+        offsets = {
+          {
+            filetype = 'neo-tree',
+            text = 'Neo-tree',
+            highlight = 'Directory',
+            text_align = 'left',
+          },
+        },
+      },
+    }
     vim.keymap.set('n', '<leader>bo', '<CMD>BufferLineCloseOthers<CR>', { noremap = true, silent = true, desc = 'Close Other Buffers' })
     vim.keymap.set('n', '<leader>br', '<CMD>BufferLineCloseRight<CR>', { noremap = true, silent = true, desc = 'Close Buffers To The Right' })
   end,
