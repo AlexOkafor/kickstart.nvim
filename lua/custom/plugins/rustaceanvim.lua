3dec52bd073daaf2dd0432d2a2bf2d2db287893d
Alex Okafor <alex@paradeofrain.com>
2024-08-02 22:59:31 -0400

    feat: get rust-analyzer to save check to a temp dir

--- lua/custom/plugins/rustaceanvim.lua
+++ lua/custom/plugins/rustaceanvim.lua
@@ -1,3 +1,19 @@
+vim.g.rustaceanvim = {
+  -- LSP configuration
+  server = {
+    default_settings = {
+      -- rust-analyzer language server configuration
+      ['rust-analyzer'] = {
+        checkOnSave = {
+          enable = true,
+          command = 'check',
+          extraArgs = { '--target-dir', '/tmp/rust-analyzer-check' },
+        },
+      },
+    },
+  },
+}
+
 return {
   'mrcjkb/rustaceanvim',
   version = '^5', -- Recommended
