bce7c2c45cefb378807127231c8121ee02787416
Alex Okafor <alex@paradeofrain.com>
2025-05-12 18:07:13 -0400

    feat: allow for custom snippets

--- init.lua
+++ init.lua
@@ -786,6 +786,8 @@ require('lazy').setup({
       local luasnip = require 'luasnip'
       luasnip.config.setup {}
 
+      require('luasnip.loaders.from_lua').lazy_load { paths = { vim.fn.stdpath 'config' .. '/snippets' } }
+
       cmp.setup {
         snippet = {
           expand = function(args)
---
+++ snippets/rust/test.lua
@@ -0,0 +1,30 @@
+local ls = require 'luasnip'
+local s = ls.snippet
+local t = ls.text_node
+
+return {
+  s('mod_test', {
+    t(vim.split(
+      [[
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use crate::create_app;
+    use bevy::app::App;
+
+    fn setup_test() -> App {
+        let mut app = create_app();
+        app
+    }
+
+    #[test]
+    fn my_test() {
+        let mut app = setup_test();
+    }
+}
+]],
+      '\n'
+    )),
+  }),
+}
